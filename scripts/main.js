1ccbf5732df750f2586fcaea5fc65e5e11f2de3b
JanHrybZSTI <73634494+JanHrybZSTI@users.noreply.github.com>
2021-02-28 11:48:56 +0100

    made fully hamburger :)

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,95 @@
+function hamburger() {
+  //variables
+  let checkBox = document.querySelector(".nav__hamburger-checkbox");
+  let hamburgerX = document.getElementById("hamburgerX");
+  let hamburgerDefault = document.getElementById("hamburgerDefault");
+
+  //setting both stylesheets to disabled
+  hamburgerX.disabled = true;
+  hamburgerDefault.disabled = true;
+
+  //rest of code
+  checkBox.addEventListener("change", () => {
+    //console.log("is check box checked: " + checkBox.checked);
+    if (checkBox.checked == true) {
+      hamburgerX.disabled = false;
+      hamburgerDefault.disabled = true;
+    } else if (checkBox.checked == false) {
+      hamburgerX.disabled = true;
+      hamburgerDefault.disabled = false;
+    }
+  });
+}
+
+function resizeHamburger() {
+  let checkBox = document.querySelector(".nav__hamburger-checkbox");
+  let hamburgerX = document.getElementById("hamburgerX");
+  let hamburgerDefault = document.getElementById("hamburgerDefault");
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 1107) {
+      hamburgerX.disabled = true;
+      hamburgerDefault.disabled = true;
+      checkBox.checked = false;
+    }
+  });
+}
+resizeHamburger();
+hamburger();
+
+/*
+window.addEventListener("resize", function () {
+  let menu = document.querySelector(".nav__menu");
+  let logo = document.querySelector(".nav__logo-box");
+  console.log("menu width: " + menu.offsetWidth);
+  console.log("logo width: " + logo.offsetWidth);
+});*/
+/*
+function changeColourOfNavbar() {
+  window.addEventListener("scroll", () => {
+    let nav = document.querySelector(".nav");
+    let currentHeight = window.scrollY;
+    console.log(currentHeight);
+    if (currentHeight > 1) {
+      //window.scrollTo(0, 600);
+      //nav.style.backgroundColor = "white";
+      //nav.style.backdropFilter = "blur(10px)";
+    } else {
+      //nav.style.backgroundColor = "transparent";
+      //nav.style.backdropFilter = "blur(0px)";
+    }
+  });
+}
+changeColourOfNavbar();
+*/
+
+//rest of unnecessary code :)
+
+/*
+let offer = document.querySelectorAll(".main__offer");
+let offerDesc = document.querySelectorAll(".main__offer-description");
+for (let i = 0; i < offer.length; i++) {
+  offer[i].addEventListener("mouseover", () => {
+    offerDesc[i].style.display = "block";
+  });
+  offer[i].addEventListener("mouseout", () => {
+    offerDesc[i].style.display = "none";
+  });
+}
+*/
+
+//function to make nav transparent at the to of 0
+/*
 function changeColourOfNavbar() {
   window.addEventListener("scroll", () => {
     let nav = document.querySelector(".nav");
     let currentHeight = window.scrollY;
     if (currentHeight > 0) {
       nav.style.backgroundColor = "white";
-      /*nav.style.backdropFilter = "blur(10px)";*/
+      //nav.style.backdropFilter = "blur(10px)";
     } else {
       nav.style.backgroundColor = "transparent";
-      /*nav.style.backdropFilter = "blur(0px)";*/
+      //nav.style.backdropFilter = "blur(0px)";
     }
   });
-}
-
-function hamburger() {
-  //variables
-  let hamburger = document.querySelector(".nav__hamburger");
-  let hamburgerItems = document.querySelector(".nav__hamburger-item");
-  let firstLine = document.querySelector(".nav__hamburger-item--first");
-  let secondLine = document.querySelector(".nav__hamburger-item--second");
-  let thirdLine = document.querySelector(".nav__hamburger-item--third");
-  let checkBox = document.querySelector(".nav__hamburger-checkbox");
-  //rest of code
-  checkBox.addEventListener("click", () => {
-    console.log("is check box checked: " + checkBox.checked);
-    if (checkBox.checked == true) {
-      /*remove*/ /*git*/
-      hamburger.classList.remove("nav__hamburger-default");
-      hamburgerItems.classList.remove("nav__hamburger-item-default");
-      firstLine.classList.remove("nav__hamburger-item--first-default");
-      secondLine.classList.remove("nav__hamburger-item--second-default");
-      thirdLine.classList.remove("nav__hamburger-item--third-default");
-      /*add*/
-      hamburger.classList.add("nav__hamburger-X");
-      hamburgerItems.classList.add("nav__hamburger-item-X");
-      firstLine.classList.add("nav__hamburger-item--first-X");
-      secondLine.classList.add("nav__hamburger-item--second-X");
-      thirdLine.classList.add("nav__hamburger-item--third-X");
-    } else if (checkBox.checked == false) {
-      /*remove*/
-      hamburger.classList.remove("nav__hamburger-X");
-      hamburgerItems.classList.remove("nav__hamburger-item-X");
-      firstLine.classList.remove("nav__hamburger-item--first-X");
-      secondLine.classList.remove("nav__hamburger-item--second-X");
-      thirdLine.classList.remove("nav__hamburger-item--third-X");
-      /*add*/
-      hamburger.classList.add("nav__hamburger-default");
-      hamburgerItems.classList.add("nav__hamburger-item-default");
-      firstLine.classList.add("nav__hamburger-item--first-default");
-      secondLine.classList.add("nav__hamburger-item--second-default");
-      thirdLine.classList.add("nav__hamburger-item--third-default");
-    }
-  });
-}
-
-hamburger();
-changeColourOfNavbar();
+  changeColourOfNavbar();
+}*/
